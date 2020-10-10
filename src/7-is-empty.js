4431dc34e8dbb342118a6bf5e5d3010d552b32af
Mats Johansen <johansenmats89@gmail.com>
2020-10-10 13:45:08 +0200

    done

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -2,7 +2,9 @@ export const isEmpty = (stringArrayOrObject) => {
   const type = typeof stringArrayOrObject
 
   if (type === 'string') {
-    return stringArrayOrObject === ''
+    return stringArrayOrObject === '';
+  } else if (type === 'object') {
+    return Object.keys(stringArrayOrObject).length === 0;
   }
 
   return false
